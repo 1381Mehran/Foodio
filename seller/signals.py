2a20b2646378353e22c3cc8fae893b36b1543960
Mehran Javid <mehranmm500@gmail.com>
2024-04-05 20:18:55 +0330

    add first task to celery

--- seller/signals.py
+++ seller/signals.py
@@ -9,5 +9,7 @@ from .tasks import check_seller_active
 
 @receiver(post_save, sender=Seller)
 def seller_active_checker(sender, instance, created, **kwargs):
+
     if created:
-        check_seller_active.apply_async(args=(instance, created), eta=instance.created_at + timedelta(minutes=1))
+
+        check_seller_active.apply_async(args=(instance.id,), eta=instance.created_at + timedelta(minutes=30))
--- seller/tasks.py
+++ seller/tasks.py
@@ -3,11 +3,15 @@ from datetime import timedelta
 from celery import shared_task
 
 from extensions.loggers import logger_info
+from .models import Seller
 
 
 @shared_task
-def check_seller_active(instance, created):
-    if instance and created:
+def check_seller_active(pk):
+
+    if pk:
+        instance = Seller.objects.get(pk=pk)
+
         if (instance.created_at + timedelta(minutes=1) and
            instance.is_active is False and instance.not_confirmed_cause is None):
 
