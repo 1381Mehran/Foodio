9c0df30b8c45cf076ad597dc60f2658aae78d7f9
Mehran Javid <mehranmm500@gmail.com>
2024-04-12 15:19:44 +0330

    add email service to proj

--- foodio/settings.py
+++ foodio/settings.py
@@ -259,4 +259,11 @@ LOGGING = {
 
 }
 
+# --------Relating to Email---------
 
+EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
+EMAIL_HOST = "smtp.gmail.com"
+EMAIL_USE_TLS = True
+EMAIL_PORT = 587
+EMAIL_HOST_USER = os.getenv('EMAIL_HOST_USER')
+EMAIL_HOST_PASSWORD = os.getenv("EMAIL_HOST_PASSWORD")
