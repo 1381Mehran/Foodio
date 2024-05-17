1b3b39d320009ec20f230bbe530a741db783b153
Mehran Javid <mehranmm500@gmail.com>
2024-05-17 19:40:34 +0330

    changes on Pagination

--- extensions/paginations.py
+++ extensions/paginations.py
@@ -5,7 +5,9 @@ from rest_framework.response import Response
 
 
 class CustomPagination(PageNumberPagination):
-    page_size = settings.PAGE_SIZE
+    page_size_query_param = 'size'
+    page_size = int(settings.PAGE_SIZE)
+    page_query_param = 'page'
 
     def get_paginated_response(self, data):
         return Response({
@@ -13,7 +15,7 @@ class CustomPagination(PageNumberPagination):
                 'total_items': self.page.paginator.count,
                 'current_page': self.page.number,
                 'last_page':  self.page.paginator.num_pages,
-                'page_size': self.page_size
+                'page_size': self.get_page_size(self.request),
             },
             'data': data
         })
